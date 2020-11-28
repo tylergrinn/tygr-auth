9fe1f219891de5010c661f622ed824bb86cd6cb0
Tyler Grinn <tylergrinn@gmail.com>
2020-11-27 23:03:18 -0500

    Removed null coalescing

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -3,7 +3,11 @@ import { actions, AuthContext } from '../store';
 
 export default function User(props: PropsWithChildren<{}>) {
   const { children } = props;
-  const [state, dispatch] = useContext(AuthContext);
+  const [{ user }, dispatch] = useContext(AuthContext);
+
+  if (!user) {
+    return <p>Not logged in</p>;
+  }
 
   const deleteAccount = () => {
     if (confirm('Are you sure you want to delete your account?'))
@@ -14,19 +18,20 @@ export default function User(props: PropsWithChildren<{}>) {
     <div className="tygr-user">
       {children || (
         <div className="default">
-          <h3>Welcome, {state.user?.email}!</h3>
+          <h3>Welcome, {user.email}!</h3>
           <p>
             Signed in
             {(() => {
-              switch (state.user?.provider || 'local') {
-                case 'local':
-                  return ' locally';
+              switch (user.provider) {
                 case 'google':
                   return ' with Google';
                 case 'twitter':
                   return ' with Twitter';
                 case 'github':
                   return ' with Github';
+                case 'local':
+                default:
+                  return ' locally';
               }
             })()}
           </p>
