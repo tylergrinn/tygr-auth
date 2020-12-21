6191d0aaa7a90f4c32208f7700ef037ef7eb2a6e
Tyler Grinn <tylergrinn@gmail.com>
2020-12-20 19:04:58 -0500

    Optimized page transition

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import AuthComponent, { AuthProps } from './Auth';
-import TygrBoundary from './components/ErrorBoundary';
+import ErrorBoundary from './components/ErrorBoundary';
 import useAuthStore, { actions, AuthContext } from './store';
 
 export { useAuthStore, actions, AuthContext };
 
 export default function Auth(props: AuthProps) {
   return (
-    <TygrBoundary>
+    <ErrorBoundary>
       <AuthComponent {...props} />
-    </TygrBoundary>
+    </ErrorBoundary>
   );
 }
